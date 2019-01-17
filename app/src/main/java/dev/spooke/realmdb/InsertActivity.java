8eaed053956a2dcdb58c51fe77385297dfbcb935
dkpachiappan <developerdeepan@gmail.com>
2019-01-17 17:59:07 +0530

    second Commit
    What's New:
    1.Inside the InsertView activity ID is auto generated

--- app/src/main/java/dev/spooke/realmdb/InsertActivity.java
+++ app/src/main/java/dev/spooke/realmdb/InsertActivity.java
@@ -2,16 +2,19 @@ package dev.spooke.realmdb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import dev.spooke.realmdb.DB.PersonRealmDb;
 import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class InsertActivity extends AppCompatActivity {
     Button btn_insert_data;
     EditText edt_email,edt_name,edt_id;
+    String str_id_increment;
     Realm mRealm;
 
     @Override
@@ -23,8 +26,7 @@ public class InsertActivity extends AppCompatActivity {
         edt_email=(EditText)findViewById(R.id.edt_email);
         edt_name=(EditText)findViewById(R.id.edt_name);
 
-        Realm.init(this);
-        mRealm=Realm.getDefaultInstance();
+        databaseInit();
 
         btn_insert_data.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,10 +38,19 @@ public class InsertActivity extends AppCompatActivity {
                 obj.setName(edt_name.getText().toString());
                 obj.setEmail(edt_email.getText().toString());
                 mRealm.commitTransaction();
-                edt_id.setText("");
+                databaseInit();
+                edt_id.setText(""+str_id_increment);
                 edt_email.setText("");
                 edt_name.setText("");
             }
         });
+    }
+
+    public void databaseInit(){
+        Realm.init(this);
+        mRealm=Realm.getDefaultInstance();
+        RealmResults<PersonRealmDb> result=mRealm.where(PersonRealmDb.class).findAll();
+        str_id_increment=Integer.toString(result.last().getId()+1);
+        edt_id.setText(str_id_increment);
     }
 }
